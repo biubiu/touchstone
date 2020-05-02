fcc657b1ad634b754f7db9afcac31f83780716e5
Shangfei <c.shangfei@gmail.com>
2020-05-03 08:21:16 +1000

    linkedHashMap as LRU

--- src/test/java/com/shawn/touchstone/di/Reflection.java
+++ src/test/java/com/shawn/touchstone/di/Reflection.java
@@ -1,5 +1,8 @@
 package com.shawn.touchstone.di;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
 import org.junit.Test;
 
 public class Reflection {
@@ -9,5 +12,17 @@ public class Reflection {
     String a = "123";
     Object inta = Integer.parseInt(a);
     System.out.println((Integer)inta);
+  }
+
+  @Test
+  public void testLinkedList() {
+    LinkedList<Integer> l = new LinkedList<>();
+    l.add(1);
+    l.add(2);
+    l.add(3);
+    assertEquals(3, l.pollLast().intValue());
+    assertEquals(2, l.size());
+    assertEquals(1, l.pop().intValue());
+    assertEquals(1, l.size());
   }
 }
