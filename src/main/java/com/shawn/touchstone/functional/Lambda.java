baec5ea33c73e240c64b02419190ef2b95d34fb7
Shangfei <shangfei.cao@covata.com>
2021-04-14 22:48:55 +1000

    lb

--- src/main/java/com/shawn/touchstone/functional/Lambda.java
+++ src/main/java/com/shawn/touchstone/functional/Lambda.java
@@ -1,14 +1,30 @@
 package com.shawn.touchstone.functional;
 
+import com.google.common.collect.Lists;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.function.DoubleBinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
 
 public class Lambda {
 
@@ -18,7 +34,8 @@ public class Lambda {
 
         private final String operation;
         private final DoubleBinaryOperator op;
-
+        private static final Map<String, Operation> stringToEnum =
+                Stream.of(values()).collect(toMap(Object::toString, e -> e));
         Operation(String operation, DoubleBinaryOperator op) {
             this.operation = operation;
             this.op = op;
@@ -27,6 +44,27 @@ public class Lambda {
         public double apply(double x, double y) {
             return op.applyAsDouble(x, y);
         }
+    }
+
+    public void freqMapping(File file) {
+        Map<String, Long> freq = new HashMap<>();
+        try(Stream<String> words = new Scanner(file).delimiter().splitAsStream("")) {
+//            words.forEach(word -> {
+//                freq.merge(word.toLowerCase(), 1L, Long::sum);
+//            });
+        freq = words.collect(groupingBy(String::toLowerCase, counting()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<String> topTen = freq.keySet().stream().sorted(Comparator.comparing(freq::get).reversed()).limit(10)
+                .collect(Collectors.toList());
+    }
+
+    public void mergeMap() {
+        List<String> arr = Lists.newArrayList("anc", "cna", "nnn");
+        arr.stream().collect(toMap(str -> str, val -> 1, Integer::sum, TreeMap::new)).forEach((k, v) -> System.out.println(k + " " + v));
+        arr.stream().collect(groupingBy(w -> alphabetize(w))).forEach((k, v) -> System.out.println(k + " " + v));
+        System.out.println(arr.stream().collect(Collectors.joining(",")));
     }
 
     public void anagrams() {
@@ -42,5 +80,20 @@ public class Lambda {
         char[] a = word.toCharArray();
         Arrays.sort(a);
         return new String(a);
+    }
+
+    public static <E> Stream<List<E>> of(List<E> list) {
+        return Stream.concat(Stream.of(Collections.emptyList()), prefixes(list).flatMap(l -> suffixes(l)));
+    }
+
+    private static <E> Stream<List<E>> prefixes(List<E> list) {
+        return IntStream.rangeClosed(1, list.size()).mapToObj(end -> list.subList(0, end));
+    }
+
+    private static <E> Stream<List<E>> suffixes(List<E> list) {
+        return IntStream.rangeClosed(0, list.size()).mapToObj(start -> list.subList(start, list.size()));
+    }
+    public static void main(String[] args) {
+        new Lambda().mergeMap();
     }
 }
