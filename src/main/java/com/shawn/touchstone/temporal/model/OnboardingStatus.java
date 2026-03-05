7f5e73a75edaf964584f221b64a7a5f389ab75a5
shang <shang@shangs-MacBook-Air.local>
2026-03-05 22:02:19 +1100

    feat: add Temporal onboarding data models

---
+++ src/main/java/com/shawn/touchstone/temporal/model/OnboardingStatus.java
@@ -0,0 +1,8 @@
+package com.shawn.touchstone.temporal.model;
+
+public enum OnboardingStatus {
+    PENDING_EMAIL_CONFIRMATION,
+    PENDING_ACCOUNT_CREATION,
+    PENDING_FOLLOWUP,
+    COMPLETED
+}
